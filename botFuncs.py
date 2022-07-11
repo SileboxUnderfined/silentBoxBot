5cc58ddf2e7465c3c7e3debd5d32cd2d83bc450e
SileboxUnderfined <valerkafm@gmail.com>
2022-07-11 20:47:13 +0300

    Update botFuncs.py

--- botFuncs.py
+++ botFuncs.py
@@ -4,25 +4,30 @@ from io import BytesIO
 from os import environ as envv
 import requests
 
-def checkAllownessOfMessage(bs,peer_id,group_id):
-    result = bs.messages.isMessagesFromGroupAllowed(user_id=peer_id,group_id=group_id)['is_allowed']
+def checkAllownessOfMessageUser(bs,user_id,group_id):
+    result = bs.messages.isMessagesFromGroupAllowed(user_id=user_id,group_id=group_id)['is_allowed']
     if result == 1: return True
     else: return False
 
+def checkAllownessOfMessagePeer(bs,peer_id,group_id):
+    items = bs.messages.getConversationMembers(peer_id=peer_id,group_id=group_id)['items']
+    for item in items:
+        if item['member_id'] == group_id: result = item['is_admin']
+
+    return result
+
 def sendMessage(bs,message,peer_id, attach=None):
-    if checkAllownessOfMessage(bs,peer_id,envv['GROUP_ID']):
-        if attach == None: bs.messages.send(message=message,peer_id=peer_id,random_id=get_random_id())
-        else: bs.messages.send(message=message,attachment=attach,peer_id=peer_id,random_id=get_random_id())
-    else: print('невозможно отправить сообщение')
+    if '2000000000' in peer_id: allowed = checkAllownessOfMessagePeer(bs,peer_id,envv['GROUP_ID'])
+    else: allowed = checkAllownessOfMessageUser(bs,peer_id,envv['GROUP_ID'])
+    if not allowed: print('невозможно отправить сообщение')
+
+    if attach == None: bs.messages.send(message=message,peer_id=peer_id,random_id=get_random_id())
+    else: bs.messages.send(message=message,attachment=attach,peer_id=peer_id,random_id=get_random_id())
 
 def getAttach(owner_id,photo_id):
     return f"photo{owner_id}_{photo_id}"
 
 def createImageCitation(forwarded_message,vupl,bs, peer_id):
-    if not checkAllownessOfMessage(bs,peer_id,envv['GROUP_ID']):
-        print('невозможно отправить фотографию и сообщение, нет разрешения')
-        return
-
     getAuthor = bs.users.get(user_ids=forwarded_message['from_id'], fields="photo_200")[0]
     author = getAuthor['first_name'] + getAuthor['last_name']
     avatar = BytesIO(requests.get(getAuthor['photo_200']).content)
