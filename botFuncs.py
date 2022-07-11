2bd32c45166f628e2b5cad95c861c17d8e98d39a
SileboxUnderfined <valerkafm@gmail.com>
2022-07-11 20:21:18 +0300

    Update botFuncs.py

--- botFuncs.py
+++ botFuncs.py
@@ -1,20 +1,28 @@
 from vk_api.utils import get_random_id
-from vk_api.exceptions import ApiError
 from imageCitate import ImageCitate
 from io import BytesIO
+from os import environ as envv
 import requests
 
+def checkAllownessOfMessage(bs,peer_id,group_id):
+    result = bs.messages.isMessagesFromGroupAllowed(user_id=peer_id,group_id=group_id)[0]['is_allowed']
+    if result == 1: return True
+    else: return False
+
 def sendMessage(bs,message,peer_id, attach=None):
-    try:
+    if checkAllownessOfMessage(bs,peer_id,envv['GROUP_ID']):
         if attach == None: bs.messages.send(message=message,peer_id=peer_id,random_id=get_random_id())
         else: bs.messages.send(message=message,attachment=attach,peer_id=peer_id,random_id=get_random_id())
-    except ApiError as error:
-        print('Невозможно отправить сообщение: ', error)
+    else: print('невозможно отправить сообщение')
 
 def getAttach(owner_id,photo_id):
     return f"photo{owner_id}_{photo_id}"
 
 def createImageCitation(forwarded_message,vupl,bs, peer_id):
+    if not checkAllownessOfMessage(bs,peer_id,envv['GROUP_ID']):
+        print('невозможно отправить фотографию и сообщение, нет разрешения')
+        return
+
     getAuthor = bs.users.get(user_ids=forwarded_message['from_id'], fields="photo_200")[0]
     author = getAuthor['first_name'] + getAuthor['last_name']
     avatar = BytesIO(requests.get(getAuthor['photo_200']).content)
@@ -27,10 +35,7 @@ def createImageCitation(forwarded_message,vupl,bs, peer_id):
     r.save(buffer, 'jpeg')
     buffer.seek(0)
 
-    try:
-        result = vupl.photo_messages(buffer, peer_id)[0]
-    except ApiError as error:
-        print('невозможно отправить фотографию ', error)
+    result = vupl.photo_messages(buffer, peer_id)[0]
     attachment = getAttach(result['owner_id'],result['id'])
 
     sendMessage(bs,message="Готово, держи", attach=attachment, peer_id=peer_id)
